80f1b04635ab17dfbab70dfe3495a0ac6b506a4e
root <root@centos8.linuxvmimages.local>
2021-02-20 17:49:47 +0100

    deploy final-app on a GCP VM

--- terraform/single_istance_vm/start_app.sh
+++ terraform/single_istance_vm/start_app.sh
@@ -3,19 +3,28 @@
 sudo apt-get update
 sudo apt-get install -yq build-essential python-pip rsync
 pip install flask
-#pip install gunicorn==19.1.1
-#pip install prometheus_client
+pip install gunicorn==19.1.1
+pip install prometheus_client==0.9.0
 
 
-cat > app.py << EOF
-from flask import Flask
+
+cat > main.py << EOF
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
+
 app = Flask(__name__)
+NUM_REQUESTS = Counter("num_requests", "Number of requests", ["endpoint"])
+
 @app.route('/')
-def hello_cloud():
-    return 'Hello World! This is Alessio first Cloud GCP web-application!'
-app.run(host='0.0.0.0')
+def hello_world():
+    NUM_REQUESTS.labels("/").inc()
+    return 'Hello World from Alessio GCP single istance VM!'
 
-EOF
+@app.route("/metrics")
+def requests_count():
+    res = []
+    res.append(generate_latest(NUM_REQUESTS))
+    return Response(res, mimetype="text/plain")
 
-python app.py
-#gunicorn -w 1 -b 0.0.0.0:8080 main:app
+#python app.py
+gunicorn -w 1 -b 0.0.0.0:8080 main:app
