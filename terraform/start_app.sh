a75e149eb0111b7b9502a8953eca4bd198e73927
root <root@centos8.linuxvmimages.local>
2021-02-20 16:17:57 +0100

    terraform apply

--- terraform/start_app.sh
+++ terraform/start_app.sh
@@ -3,9 +3,11 @@
 sudo apt-get update
 sudo apt-get install -yq build-essential python-pip rsync
 pip install flask
+pip install gunicorn==19.1.1
+pip install prometheus_client
 
 
-cat > app.py << EOF
+cat > main.py << EOF
 from flask import Flask
 app = Flask(__name__)
 
@@ -17,4 +19,4 @@ app.run(host='0.0.0.0')
 
 EOF
 
-python app.py
+gunicorn -w 1 -b 0.0.0.0:8080 main:app
