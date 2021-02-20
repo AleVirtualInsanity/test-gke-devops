4b7406044f018d7ca6cf771d021a4b6a22ef7736
root <root@centos8.linuxvmimages.local>
2021-02-20 17:24:04 +0100

    regression

---
+++ terraform/start_app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -yq build-essential python-pip rsync
+pip install flask
+#pip install gunicorn==19.1.1
+#pip install prometheus_client
+
+
+cat > main.py << EOF
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_cloud():
+    return 'Hello World! This is Alessio first Cloud GCP web-application!'
+app.run(host='0.0.0.0')
+
+EOF
+
+python app.py
+#gunicorn -w 1 -b 0.0.0.0:8080 main:app
