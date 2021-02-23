74bcaa24dd4b012a82f7a971964b4f228a362b56
root <root@centos8.linuxvmimages.local>
2021-02-23 18:05:36 +0100

    test2-log

--- app/main.py
+++ app/main.py
@@ -9,6 +9,11 @@ def hello_world():
     NUM_REQUESTS.labels("/").inc()
     return 'Hello World from Alessio GKE cluster!'
 
+@app.route("/ntt")
+def log():
+    NUM_REQUESTS.labels("/ntt").inc()
+    return 'Hello, World - 2!'
+
 @app.route("/metrics")
 def requests_count():
     res = []
